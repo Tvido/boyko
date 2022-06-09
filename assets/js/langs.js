b804f551864244efea1c55e6bff164fef05bfeec
Volodymyr Viter <mad.tvido@gmail.com>
2022-06-09 23:49:21 +0300

    upd

--- assets/js/langs.js
+++ assets/js/langs.js
@@ -13,19 +13,41 @@
 /*==================== TOGGLE LANGUAGE ====================*/
 
 const langs = document.querySelector('.langs'),
-  link = document.querySelectorAll('a'),
-  home = document.querySelector('.home'),
-  genres = document.querySelector('.genres');
+  link = document.querySelectorAll('a');
+home = document.querySelector('.home');
+genres = document.querySelector('.genres');
+methods = document.querySelector('.methods');
+galleries = document.querySelector('.galleries');
+testimonials = document.querySelector('.testimonials');
+contactme = document.querySelector('.contactme');
+home__title = document.querySelector('.home__title');
+home__subtitle = document.querySelector('.home__subtitle');
+home__description = document.querySelector('.home__description');
+experience = document.querySelector('.experience');
+projects = document.querySelector('.projects');
+compilations = document.querySelector('.compilations');
 
 link.forEach(el => {
   el.addEventListener('click', () => {
+    // if (langs.querySelector !== 'active') {
     langs.querySelector('.active').classList.remove('active');
     el.classList.add('active');
+    // }
 
     let attr = el.getAttribute('language');
 
     home.textContent = data[attr].home;
     genres.textContent = data[attr].genres;
+    methods.textContent = data[attr].methods;
+    galleries.textContent = data[attr].galleries;
+    testimonials.textContent = data[attr].testimonials;
+    contactme.textContent = data[attr].contactme;
+    home__title.textContent = data[attr].home__title;
+    home__subtitle.textContent = data[attr].home__subtitle;
+    home__description.textContent = data[attr].home__description;
+    experience.textContent = data[attr].experience;
+    projects.textContent = data[attr].projects;
+    compilations.textContent = data[attr].compilations;
   });
 });
 
@@ -33,10 +55,32 @@ const data = {
   us: {
     home: 'Home',
     genres: 'Genres',
+    methods: 'Methods',
+    galleries: 'Galleries',
+    testimonials: 'Testimonials',
+    contactme: 'Contact Me',
+    home__title: 'Myroslava',
+    home__subtitle: 'Photographer',
+    home__description:
+      'Photography captures eternity, is proof that everything is only a brief moment in the eyes of eternity. In my photos, I want to convey the authenticity of this moment, the feeling of this moment, because this time is already history!',
+    experience: 'Years experience',
+    projects: 'Completed projects',
+    compilations: 'Compilations done',
   },
   ua: {
     home: 'Домашня',
     genres: 'Жанри',
+    methods: 'Методи',
+    galleries: 'Галереї',
+    testimonials: 'Відгуки',
+    contactme: 'Контакти',
+    home__title: 'Мирослава',
+    home__subtitle: 'Фотограф',
+    home__description:
+      'Фотографія фіксує вічність, є доказом того, що все лиш коротка мить в очах вічності. В своїх фотографіях , хочу донести справжність цієї миті,відчуття цього моменту, адже цей час вже є історією!',
+    experience: 'Років досвіду',
+    projects: 'Завершених проектів',
+    compilations: 'Спільних проектів',
   },
 };
 
